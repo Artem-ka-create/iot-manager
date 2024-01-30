2b1ee58c9fa24150b227c727e94120cf7a1bd3ec
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2024-01-30 23:19:37 +0100

    prime fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,5 @@ import './index.css'
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
