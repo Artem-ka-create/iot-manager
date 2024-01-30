b714c3cd237689e1630f3c34f6777b0f6bfc83c1
Vladyslav Moskalenko <vladkiew15@gmail.com>
2024-01-30 23:28:32 +0100

    add

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import styles from './Header.module.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFile, faHdd, faHome} from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
 
@@ -8,11 +10,16 @@ function Header() {
     <div>
 
         <div className={styles.logo}>
-            <div className={styles.logo__name}></div>
-            <div className={styles.logo_additional}></div>
+            <div className={styles.logo__name}>TUKE</div>
+            <div className={styles.logo_additional}>Solaris | B526</div>
         </div>
 
-        <div></div>
+        <div className={styles.menu}>
+            <div className={styles.menu__item}>
+                <div className={styles.menu__item__icon}><FontAwesomeIcon icon={faFile}/></div>
+                <div className={styles.menu__item__name}>Home</div>
+            </div>
+        </div>
 
         <div></div>
 
